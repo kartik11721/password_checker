4546ae12afbd70cb3384f675f373b874938c28f4
Kartik Kumar <83923172+kartik11721@users.noreply.github.com>
2021-06-15 17:43:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ print('''\n-----!! Your Password Must Have !!-----
 5. At least 1 character from [ _ or @ or $ ].\n''')
 
 password = input('Enter a password : ')
-invalid = False
+valid = True
 print('\n')
 
 if (len(password)<8):
-    invalid = True
+    valid = False
     print('- password is too short')
 if not re.search("[a-z]", password):
-    invalid = True
+    valid = False
     print('- atleast 1 lower case character is required')
 if not re.search("[A-Z]", password):
-    invalid = True
+    valid = False
     print('- atleast 1 upper case character is required')
 if not re.search("[0-9]", password):
-    invalid = True
+    valid = False
     print('- atleast 1 number [0-9] is required')
 if not re.search("[_@$]", password):
-    invalid = True
+    valid = False
     print('- atleast 1 special character i.e _, @, $ is required')
 if re.search("\s", password):
-    invalid = True
+    valid = False
     print('- password can not contain spaces')
-if invalid == False:
+if valid == True:
     print("Valid Password")
